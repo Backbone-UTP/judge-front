a6af12651fd22e3c31e2324b085670b3b7ae7f53
Juan Jose Jaramillo Tamayo <juanjotam098@gmail.com>
2026-04-19 20:55:26 -0500

    feat: configure routing and feature module for challenge-workspace

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'challenge-workspace',
+        pathMatch: 'full'
+    },
+    {
+        path: 'challenge-workspace',
+        loadChildren: () => 
+            import('./features/challenge-workspace/challenge-workspace').then(m => m.challengeWorkspaceRoutes)
+    }
+];
---
+++ src/app/features/challenge-workspace/challenge-workspace.ts
@@ -0,0 +1,2 @@
+export { ChallengeWorkspaceContainer } from './containers/challenge-workspace/challenge-workspace';
+export { challengeWorkspaceRoutes } from './challenge-workspace.routes';
